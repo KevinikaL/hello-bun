bbba00b1940375fc433cd419630a38e7226e0269
Kevinika Legier <k.legier2012@gmail.com>
2026-08-05 18:54:15 -0500

    print the date

--- hello.js
+++ hello.js
@@ -3,3 +3,4 @@ let message = "I am learning " + course + "!";
 console.log(message);
 console.log("This JavaScript runs on MY machine.");
 console.log("My name is Kevinika.");
+console.log("Today's date is August 5, 2026.");
